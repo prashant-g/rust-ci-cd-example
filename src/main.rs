3eb68c92c9dd05e60f8dff81037925c270b1f594
prashant-g <amarprash@gmail.com>
2026-09-01 20:46:16 +0530

    Add Rust CI pipeline2

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,3 @@ fn main() {
     println!("{} - {} = {}", a, b, subtract(a, b));
     println!("{} * {} = {}", a, b, multiply(a, b));
 }
-
-
-
-//cargo fmt --check
-//cargo fmt 
-//cargo clippy --all-targets --all-features -- -D warnings
-
-//git init 
-//git status
-//git add .
-// git commit -m "Initial Rust project"  
-//git remote add origin rust-ci-cd-example
-//git branch -M main
-//git remote set-url origin https://github.com/prashant-g/rust-ci-cd-example.git
-//git remote -v
-//git push -u origin main
